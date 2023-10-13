652a7ec105f0de309493ac6bb7268b858a0f9f2b
theau <theaudugueperoux@gmail.com>
2023-10-13 12:21:51 +0200

    :tada: portfolio base

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+        ubuntu: ['Ubuntu', 'sans-serif']
+      },
+    },
+  },
+  plugins: [],
+}
